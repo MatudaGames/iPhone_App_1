077445cf82d9986e2451746ed474c626a3e27a90
Kristaps <mtdr@inbox.lv>
2012-05-12 22:23:33 +0300

    Initial Commit

---
+++ testapp1/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  testapp1
+//
+//  Created by Kristaps Auzins on 5/12/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ testapp1/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  testapp1
+//
+//  Created by Kristaps Auzins on 5/12/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
